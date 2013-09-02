f290a5d47775b0a401513703ae995d788adf0227
Swaroop SM <swaroop.striker@gmail.com>
2013-09-02 22:37:30 +0530

    added fetch method

--- lib/doattend.rb
+++ lib/doattend.rb
@@ -1,5 +1,19 @@
 require "doattend/version"
+require "rest-client"
+require "yaml"
 
 module Doattend
-  # Your code goes here...
+
+  # Fetch data from DoAttend API.
+  def self.fetch
+  	config = YAML.load("#{Rails.root}/config.doattend.yml")
+  	url = "http://doattend.com/api/events/#{conf[:event]}/participants_list.json?api_key=#{conf[:key]}"
+  	begin
+  		RestClient.get(url)
+  	rescue Exception => e
+  		e
+  	end
+  	RestClient.get(url)
+  end
+
 end
