feb9d0d76de301c26c509f453c5b192cc72a5eae
Swaroop SM <swaroop.striker@gmail.com>
2013-09-02 22:00:34 +0530

    added install generator

---
+++ lib/generators/doattend/install/install_generator.rb
@@ -0,0 +1,15 @@
+require 'rails/generators'
+
+module Doregister
+		class InstallGenerator < Rails::Generators::Base
+			source_root File.expand_path('../templates', __FILE__)
+
+			class_option :e, :type => :string, :required => true, :desc => "Specify DoAttend Event ID", :banner => "YOUR_DOATTEND_EVENT_ID"
+			class_option :k, :type => :string, :required => true, :desc => "Specify DoAttend API key", :banner => "YOUR_DOATTEND_API_KEY"
+
+			def generate_config
+				template "doattend.rb", "#{Rails.root}/config/doattend.yml"
+			end
+
+		end
+end
