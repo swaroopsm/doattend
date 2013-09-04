a94aa040143ad0ba3a7d1b26096307e36e352ace
Swaroop SM <swaroop.striker@gmail.com>
2013-09-04 21:44:03 +0530

    defined constructor and fetch function in base class

--- lib/doattend/base.rb
+++ lib/doattend/base.rb
@@ -1,14 +1,29 @@
 require "rest-client"
+require "json"
 
 module Doattend
 	
 	class Base
 		
-		def self.fetch
-	  	conf = YAML.load_file("#{Rails.root}/config/doattend.yml")
-  		url = "http://doattend.com/api/events/#{conf['doattend']['event']}/participants_list.json?api_key=#{conf['doattend']['key']}"
+		attr_accessor :event, :key, :result
+
+		def initialize(e=nil, k=nil)
+			if defined? Rails
+	  		conf = YAML.load_file("#{Rails.root}/config/doattend.yml")
+	  		self.event = conf['doattend']['event']
+	  		self.key = conf['doattend']['key']
+	  	else
+	  		self.event = e
+	  		self.key = k
+	  	end
+
+		end
+		
+		# Request DoAttend and fetch results.
+		def fetch
+  		url = "http://doattend.com/api/events/#{self.event}/participants_list.json?api_key=#{self.key}"
   		begin
-  			RestClient.get(url)
+  			self.result = JSON.parse(RestClient.get(url))
   		rescue Exception => e
   			e
 			end
