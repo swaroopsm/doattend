b07c74d2dfc8025d3472d2d1815282bb79904cc1
Swaroop SM <swaroop.striker@gmail.com>
2013-09-05 00:39:52 +0530

    defined pluck method for participant class

--- lib/doattend/participant.rb
+++ lib/doattend/participant.rb
@@ -2,11 +2,21 @@ module Doattend
 	
 	class Participant
 		
-		attr_accessor :result, :info
+		attr_accessor :result, :info, :general_info
 
 		def initialize(result)
 			self.result = result
-			self.info = result['participant_information']
+			#self.info = result['participant_information']
+			self.general_info = ['Ticket_Number', 'Email', 'Date', 'Name']
+		end
+
+		# Pluck a particular field from participants
+		def pluck(field)
+			if self.general_info.include? field
+				self.result.map{ |p| p[field] }
+			else
+				self.result.map{ |p| p['participant_information'].select{ |a| a['desc'] == field } }.flatten.map{ |x| x['info'] }
+			end
 		end
 
 	end
