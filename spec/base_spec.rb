1961b183cfc5de99b24f05b1ad10803df0ae46f6
Swaroop SM <swaroop.striker@gmail.com>
2013-09-08 20:17:37 +0530

    converted it to all method names

--- spec/base_spec.rb
+++ spec/base_spec.rb
@@ -7,14 +7,18 @@ describe Doattend::Base do
 		@doattend = Doattend::Base.new
 	end
 	
-	it "should fetch data from the API" do
+	it "#fetch" do
 		sample_response = File.new(File.expand_path('../example.json', __FILE__))
 		stub_request(:get, @doattend.url).to_return(:body => sample_response)
 		@doattend.fetch
 	end
 
-	it "should return the total participants" do
+	it "#aggregate" do
 		@doattend.aggregate.should eq 11
+	end
+
+	it "#ticket" do
+		@doattend.ticket.should be_instance_of Doattend::Ticket
 	end
 
 end
