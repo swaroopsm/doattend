d292e50430550b5a416b584b138d3d7be9db69c8
Swaroop SM <swaroop.striker@gmail.com>
2013-09-22 19:14:55 +0530

    defined public folder

--- lib/doattend/web.rb
+++ lib/doattend/web.rb
@@ -5,6 +5,7 @@ module Doattend
 	
 	class Web < Sinatra::Base
 		
+		set :public_folder, File.expand_path(File.dirname(__FILE__) + '/web' )
 		set :views, File.expand_path(File.dirname(__FILE__) + "/web/views/")
 
 		get "/" do
