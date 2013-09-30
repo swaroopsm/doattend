0b1fab3b3a7d1eb10769d619bea6c04756c0e9b0
Swaroop SM <swaroop.striker@gmail.com>
2013-09-30 21:37:56 +0530

    added title and set static to true

--- lib/doattend/web.rb
+++ lib/doattend/web.rb
@@ -1,4 +1,5 @@
 require "sinatra"
+require "time"
 require_relative "base"
 
 module Doattend
@@ -7,6 +8,7 @@ module Doattend
 		
 		set :public_folder, File.expand_path(File.dirname(__FILE__) + '/web' )
 		set :views, File.expand_path(File.dirname(__FILE__) + "/web/views/")
+		set :static, true
 
 		get "/" do
 			@doattend = Doattend::Base.new
